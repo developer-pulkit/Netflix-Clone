549389a75c9e2ad411791a9fd379c6f399a95afc
Pulkit <297pulkitmehta@gmail.com>
2022-06-06 04:23:21 -0400

    Finished Main.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function navbar() {
   return (
     <div className="flex items-center justify-between p-4 z[100] w-full absolute">
-      <h1 className="text-red-600 text-4xl font-bold cursor-pointer ">
+      <h1 className="text-red-600 text-4xl font-bold cursor-pointer z-10">
         NETFLIX
       </h1>
       <div>
